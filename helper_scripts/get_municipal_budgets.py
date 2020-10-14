f678013899571f28e1443091cd9a588766cf0fc0
jicruz96 <accounts@jicruz.com>
2020-10-14 00:18:30 -0400

    Refactor get_municipal_budgets.py script

--- helper_scripts/get_municipal_budgets.py
+++ helper_scripts/get_municipal_budgets.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 from datetime import date
-from unidecode import unidecode
 from requests import get
+from unidecode import unidecode
 
 
 # List of Puerto Rico municipalities
@@ -91,33 +91,36 @@ municipios = [
 year = date.today().year
 
 # URL template
-URL = "http://ogp.pr.gov/PresupuestoMunicipal/"
-URL += "Presupuestos%20municipales%20{}%20{}/".format(year - 1, year)
-URL += "{}"
-URL += "%20{}.pdf".format(year)
+url_template = "http://ogp.pr.gov/PresupuestoMunicipal/"
+url_template += "Presupuestos%20municipales%20{}%20{}/".format(year - 1, year)
+url_template += "{}"
+url_template += "%20{}.pdf".format(year)
 
-failed_munis = []
+failed_downloads = []
 for muni in municipios:
     # Create download url. Make sure to replace spaces with %20
-    url = URL.format(unidecode(muni))   # Remove the áéíóú's and such
-    url = url.replace(" ", "%20")       # Replace spaces with "%20"
+    url = url_template.format(unidecode(muni))  # Remove the áéíóú's and such
+    url = url.replace(" ", "%20")               # Replace spaces with "%20"
 
-    # make file name
-    file_name = muni.lower().replace(" ", "_") + ".pdf"
-    file_name = unidecode(file_name)  # removes the áéíóú's and such
-
-    print(url, file_name)
-
-    # DOWNLOAD and SAVE
-    # Use a User-Agent header to mimic a browser request
+    # DOWNLOAD TIME BABY
     print("Downloading {} pdf...".format(muni))
+
+    # Use a User-Agent header to mimic a browser request
     response = get(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+    # If the download didn't work, let the user know and append the town to
+    # the failed_downloads list. We will print this at the end.
     if response.status_code != 200:
-        failed_munis.append(muni)
+        failed_downloads.append(muni)
         print("{} failed :(".format(muni))
+    # If everything went okay, save the file
     else:
-        file_content = response.content
+        # make file name
+        file_name = muni.lower().replace(" ", "_") + ".pdf"
+        file_name = unidecode(file_name)  # removes the áéíóú's and such
         with open(file_name, 'wb') as file:
-            file.write(file_content)
+            file.write(response.content)
         print("Successfully downloaded")
-print(failed_munis)
+
+# When finished, print list of failed downloads
+print(failed_downloads)
