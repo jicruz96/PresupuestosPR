ec6b17285e7299f0fef003b157f88004ac5b79ed
J.I. Cruz <58642383+jicruz96@users.noreply.github.com>
2020-11-04 19:04:23 -0600

    Add comments and doc strings to methods in index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,13 +9,20 @@ from unidecode import unidecode
 
 
 def standardize(s):
-    """ standardize """
+    """ helper function that standardizes inputs
+        * Replaces spaces and underscores with hyphens
+        * Removes commas
+        * Turns all letters to lower case
+    """
     s = s.replace(' ', '-').replace('_', '-').replace(',', '').lower()
     return unidecode(s)
 
 
 def filterByBudgetType(budget, budgetType):
-    """ filter by budget type """
+    """ 
+    Removes all budget types from a budget dictionary, except
+    for the 'municipio' and budgetType keys
+    """
 
     newDict = {}
     for key in budget:
@@ -26,48 +33,72 @@ def filterByBudgetType(budget, budgetType):
 
 
 def getDepartment(department):
-    """ gets department """
+    """
+    Takes a department keyword or ID as input, standardizes it,
+    then finds and returns the associated department
+    """
 
-    if department:
-        department = standardize(department)
+    # if no department was input, return None
+    if department is None:
+        return None
+    
+    # If input is a department name, return it
+    if department in departments:
+        return department
 
-        if department in departments:
-            return department
+    # If input is a department ID, return its department
+    if department in departmentIds:
+        return departmentIds[department]
 
-        if department in departmentIds:
-            return departmentIds[department]
+    # If input is a keyword, return associated department
+    if department in departmentKeys:
+        return departmentKeys[department]
 
-        if department in departmentKeys:
-            return departmentKeys[department]
-
+    # else, return None
     return None
-
 
 def getBudgetType(budgetType):
-    """ gets budget type """
+    """ 
+    Input validator for budgetType.
+    If budgetType is not recognized, returns None
+    """
 
-    if budgetType:
-        budgetType = standardize(budgetType)
+    # if no budget type was inputted, return None
+    if budgetType is None:
+        return None
+    
+    # standardize input
+    budgetType = standardize(budgetType)
 
-        if budgetType in ['gastos', 'ingresos', 'departamentos']:
-            return budgetType
-
+    # return budgetType if it's a recognized budget type
+    if budgetType in ['gastos', 'ingresos', 'departamentos']:
+        return budgetType
+    
+    # else, return None
     return None
 
-
 def getMuniDict(municipality, filter=None):
-    """ returns muni dict """
+    """
+    Create and return a dictionary for the municipal budget of a specific
+    municipality
+    """
 
+    # The filter is either None, a department, or a budgetType.
+    # Get those values.
     department = getDepartment(filter)
     budgetType = getBudgetType(filter)
+    # If filter isn't None, but not a department or a budget type, abort
     if budgetType is None and department is None and filter is not None:
         abort(404)
-
+        
+        
+    # Get budgetary information from JSON file
     path = "jsons/{}.json".format(municipality.replace('-', '_'))
     with open(path, 'r') as file:
         budget = load(file)
         file.close()
 
+    # If filter is a department, return department budget only
     if department:
         budget = filterByBudgetType(budget, 'gastos')
         muni = budget['municipio']
@@ -77,6 +108,7 @@ def getMuniDict(municipality, filter=None):
                 newDict['gastos'].append(row)
         return newDict
 
+    # If filter is a budget type, filter by budget type
     if budgetType:
         budget = filterByBudgetType(budget, budgetType)
     else:
@@ -87,7 +119,10 @@ def getMuniDict(municipality, filter=None):
 
 
 def getMuniBudget(municipality, keyword2):
-    """ return municipality budget """
+    """
+    wrapper function for getMuniDict that will hold logic for returning
+    data in different formats. More to come
+    """
 
     try:
         extension = municipality.split('.')[1]
@@ -108,7 +143,10 @@ def getMuniBudget(municipality, keyword2):
 def getDepartmentBudget(department):
     """ Returns budget info for a specific department """
 
+    # Get all municipalities where the department exists
     municipalities = departments[department]['municipios']
+    
+    # Get the department budget for each municipality
     departmentBudgets = {}
     for municipality in municipalities:
         muniDict = getMuniDict(municipality, department)
@@ -147,28 +185,32 @@ def getMuniCsv(muni, filter):
 @app_views.route('/', strict_slashes=False)
 def home_page():
     """ home page """
-    return render_template('home.html')
-
-
-@app_views.route('/something', strict_slashes=False)
-def something():
-    """ something """
-    return render_template('something.html', municipios=municipalities)
-
+    return render_template('home.html', municipios=municipalities)
 
 @app_views.route('/api/<keyword1>', strict_slashes=False)
 @app_views.route('/api/<keyword1>/<keyword2>', strict_slashes=False)
-def get_info(keyword1, keyword2=None):
-    """ Returns budget of muni as a JSON object """
+def main_logic(keyword1, keyword2=None):
+    """
+    Validates user inputs by evaluating the two keywords in the API.
+    Generates a response
+    """
 
+    # standardize keyword1
     keyword1 = standardize(keyword1)
 
+    # If keyword1 is a municipality, return the municipality budget
     if keyword1.split('.')[0] in municipalities:
         return jsonify(getMuniBudget(keyword1, keyword2))
 
+    # If it's not a municipality, then keyword2 must not exist
+    # Abort if it does
     if keyword2:
         abort(404)
 
+    # Keyword1 can either be:
+    #   * 'departamentos'-> returns list of departments
+    #   * 'municipios' -> returns list of municipalities
+    #   * a department name -> returns department budget for each municipality
     if keyword1 == 'departamentos':
         return jsonify(departments)
 
