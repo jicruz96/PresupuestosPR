9dc1037179a1b74cafe9632401d582c5aa9e1e6b
J.I. Cruz <58642383+jicruz96@users.noreply.github.com>
2020-11-04 19:05:11 -0600

    Change doc string in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" API """
+""" Flask instance of API runs in this module """
 
 from flask import Flask
 from flask_cors import CORS
@@ -8,13 +8,6 @@ from api.v1.views import app_views
 app = Flask("MUNICIPAL BUDGET API")
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r'/api/v1/*': {"origins": "*"}})
-
-
-@app.teardown_appcontext
-def close(dummy):
-    """ end of app """
-    pass
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5004, threaded=True, debug=True)
